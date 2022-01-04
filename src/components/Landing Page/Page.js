630e4a15aa894cdb7afb1e82fc03d568426bfd21
merin97 <merinmaryphilip@gmail.com>
2022-01-04 13:36:37 +0530

    commit

--- src/components/Landing Page/Page.js
+++ src/components/Landing Page/Page.js
@@ -6,14 +6,7 @@ function Page() {
         <div className="dark-overlay">
           <div className="landing-inner">
             <h1 className="x-large">DX Client Management</h1>
-            {/* <p className="lead">
-              Create a developer profile/portfolio, share posts and get help from
-              other developers
-            </p>
-            <div className="buttons">
-              <a href="register.html" className="btn btn-primary">Sign Up</a>
-              <a href="login.html" className="btn btn-light">Login</a>
-            </div> */}
+          
           </div>
         </div>
       </section>
